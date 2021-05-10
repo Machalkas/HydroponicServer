565446e11bf278bc36b3aa3991288c4ee04ff3cc
Machalka <al1999dk@gmail.com>
2021-05-10 14:40:03 +0300

    добавил get_latest_statistic

--- Farms/consumers.py
+++ Farms/consumers.py
@@ -22,19 +22,35 @@ class DataConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_discard(self.farm_token, self.channel_name)
     
     async def receive(self, text_data):
+        is_broadcast=False
         data=json.loads(text_data)
         action=data["action"]
         print(action)
         if action=='get_statistic':
             message = await self.get_statistic(self.farm)
             message={'statistic':json.loads(message)}
-        await self.send(text_data=json.dumps(message))
-        # await self.channel_layer.group_send(self.farm_token, {'type':'broadcast','message':message})
+        elif action=='get_latest_statistic':
+            message = await self.get_statistic(self.farm, True)
+            message={'statistic':json.loads(message)}   
+        elif action=='save_statistic':
+            options=data["options"]
+            message = await self.save_statistic(self.farm, options)
+            if not message:
+                message={'error':'fail to save statistic'}
+            else:
+                message={'sensors':options}
+            is_broadcast=True
+        else:
+            message={'error':'fail to process request'}
+        if is_broadcast:
+            await self.channel_layer.group_send(self.farm_token, {'type':'broadcast','message':message})
+        else:
+            await self.send(text_data=json.dumps(message))
 
 
     async def broadcast(self, data):
         message=data['message']
-        await self.send(text_data=json.dumps({'message':message}))
+        await self.send(text_data=json.dumps(message))
 
     @database_sync_to_async
     def get_farm(self, k):
@@ -44,14 +60,28 @@ class DataConsumer(AsyncWebsocketConsumer):
             return None
 
     @database_sync_to_async
-    def get_statistic(self, farm, from_date=None, to_date=None):
+    def get_statistic(self, farm, only_last=False, from_date=None, to_date=None):
         try:
-            st=Statistic.objects.filter(farm=farm.pk)
-            if from_date!=None:
-                st=st.filter(record_date>=from_date)
-            if to_date!=None:
-                st=st.filter(record_date<=to_date)
+            if only_last:
+                st=Statistic.objects.latest("record_date")
+                st=(st,)
+            else:
+                st=Statistic.objects.filter(farm=farm.pk)
+                if from_date!=None:
+                    st=st.filter(record_date>=from_date)
+                if to_date!=None:
+                    st=st.filter(record_date<=to_date)
             st=serialize('json', st)
             return st
         except:
             return None
+
+    @database_sync_to_async
+    def save_statistic(self, farm, sensors):
+        try:
+            sensors["farm_id"]=self.farm.pk
+            st=Statistic.objects.create(**sensors)
+            st.save()
+            return True
+        except:
+            return False
--- HydroponicServer/asgi.py
+++ HydroponicServer/asgi.py
@@ -3,6 +3,7 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
 import Farms.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HydroponicServer.settings')
